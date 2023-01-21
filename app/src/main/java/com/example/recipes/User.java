b8082c10eea1c674deafd4ded76984b121f9d8dd
rotemg-inmanage <rotemg@inmanage.net>
2023-01-21 22:32:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/recipes/User.java
@@ -0,0 +1,17 @@
+package com.example.recipes;
+
+public class User {
+    public String name;
+    public String id;
+    public String phone;
+    public String email;
+
+    public User(String name, String id, String phone, String email) {
+        this.name = name;
+        this.id = id;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public User () {}
+}
