138649bbefe70e99c0587866f87708a9652e04c1
rotemg-inmanage <rotemg@inmanage.net>
2023-01-30 19:20:49 +0200

    Login and Register activities tiny fixes

--- app/src/main/java/com/example/recipes/Login.java
+++ app/src/main/java/com/example/recipes/Login.java
@@ -1,27 +1,48 @@
 package com.example.recipes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Login extends AppCompatActivity {
 
+    private FirebaseAuth mAuth; // global
+    private EditText emailText;
+    private EditText passwordText;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    Button loginBtn;
+    TextView registerHereBtn;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-       final EditText email = findViewById(R.id.emailAddress);
-       final EditText password = findViewById(R.id.password);
-       final Button loginBtn = findViewById(R.id.loginBtn);
-       final TextView registerHereBtn = findViewById(R.id.registerHere);
+        mAuth = FirebaseAuth.getInstance(); // local
+
+        emailText = findViewById(R.id.emailAddress);
+        passwordText = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        registerHereBtn = findViewById(R.id.registerHere);
 
         registerHereBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,23 +52,30 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(Login.this, Register.class));
             }
         });
-
-       loginBtn.setOnClickListener(new View.OnClickListener() {
-
-           @Override
-           public void onClick(View v) {
-               final String emailText = email.getText().toString();
-               final String passwordText = password.getText().toString();
-
-               if (emailText.isEmpty() || passwordText.isEmpty()) {
-                   Toast.makeText(Login.this, "Please enter your email and password",
-                           Toast.LENGTH_SHORT).show();
-               }
-
-           }
-       });
-
-
     }
 
+    public void loginFunc(View view) {
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(Login.this, "Please enter your email and password",
+                           Toast.LENGTH_SHORT).show();
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "You entered successfully",
+                                    Toast.LENGTH_LONG).show();
+                            // here we need to navigate to homepage fragment
+                        } else {
+                            Toast.makeText(Login.this, "Login failed",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
 }
--- app/src/main/java/com/example/recipes/Register.java
+++ app/src/main/java/com/example/recipes/Register.java
@@ -3,6 +3,7 @@ package com.example.recipes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +29,7 @@ public class Register extends AppCompatActivity {
     private EditText passwordText;
     private EditText conPasswordText;
 
-    int id = 0;
+    long id = 0;
 
     Button registerBtn;
     TextView loginHereBtn;
@@ -54,7 +55,7 @@ public class Register extends AppCompatActivity {
         loginHereBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                startActivity(new Intent(Register.this, Login.class));
             }
         });
 
