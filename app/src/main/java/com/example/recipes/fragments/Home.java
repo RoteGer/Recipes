258d3aebf942056fa40a19bd0624d76487a8d09c
rotemg-inmanage <rotemg@inmanage.net>
2023-02-06 23:46:17 +0200

    Admin user + Home Page fragments - start of development

--- app/src/main/java/com/example/recipes/fragments/Home.java
+++ app/src/main/java/com/example/recipes/fragments/Home.java
@@ -1,5 +1,7 @@
 package com.example.recipes.fragments;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +9,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.recipes.R;
+import com.example.recipes.activities.Login;
+import com.example.recipes.activities.RecipesMainActivity;
+import com.example.recipes.activities.Register;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +22,10 @@ import com.example.recipes.R;
  * create an instance of this fragment.
  */
 public class Home extends Fragment {
+
+
+    Button recipes;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,7 +70,19 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        recipes = view.findViewById(R.id.recipesBtn);
+
+        recipes.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RecipesMainActivity.class);
+                startActivity(intent);
+            }
+
+        });
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
