60ebe4dc339a07bf70b708456cd77df742831398
rotemg-inmanage <rotemg@inmanage.net>
2023-02-09 22:00:53 +0200

    Code cleanups

--- app/src/main/java/com/example/recipes/fragments/FragmentLogin.java
+++ app/src/main/java/com/example/recipes/fragments/FragmentLogin.java
@@ -36,39 +36,14 @@ public class FragmentLogin extends Fragment {
     private EditText emailText;
     private EditText passwordText;
 
-    FirebaseDatabase database;
-    DatabaseReference myRef;
-
     Button login;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FragmentLogin() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentLogin.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentLogin newInstance(String param1, String param2) {
+    public static FragmentLogin newInstance() {
         FragmentLogin fragment = new FragmentLogin();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -76,16 +51,10 @@ public class FragmentLogin extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     private void setContentView(int activity_login) {
     }
-
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/example/recipes/fragments/Home.java
+++ app/src/main/java/com/example/recipes/fragments/Home.java
@@ -25,28 +25,14 @@ public class Home extends Fragment {
     Button recipes;
     Button personalInfo;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String isAdmin = "isAdminBundle";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String isAdminBundle;
 
-    public Home() {
-        // Required empty public constructor
-    }
+    public Home() { // Required empty public constructor
+         }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Home newInstance(String param1, String param2) {
+    public static Home newInstance(String param1) {
         Home fragment = new Home();
         Bundle args = new Bundle();
         args.putString("isAdmin", param1);
@@ -71,9 +57,11 @@ public class Home extends Fragment {
         Bundle bundle = this.getArguments();
         recipes = view.findViewById(R.id.recipesBtn);
         personalInfo = view.findViewById(R.id.personalInfoBtn);
+
         if (isAdminBundle == "false") {
             personalInfo.setVisibility(view.GONE);
         }
+
         personalInfo.setOnClickListener(new View.OnClickListener(){
 
             @Override
