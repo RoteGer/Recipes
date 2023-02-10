4f3b78406451cf3c45e8df9cac2c8a57b8edaae4
linoyhovav <linoy4578@gmail.com>
2023-02-10 13:40:25 +0200

    design and Personal Info page

--- app/src/main/java/com/example/recipes/fragments/Home.java
+++ app/src/main/java/com/example/recipes/fragments/Home.java
@@ -3,14 +3,14 @@ package com.example.recipes.fragments;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.fragment.app.Fragment;
+
+import com.example.recipes.activities.PersonalInfoActivity;
 import com.example.recipes.R;
 import com.example.recipes.activities.RecipesMainActivity;
 
@@ -63,7 +63,8 @@ public class Home extends Fragment {
 
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getActivity(), PersonalInfoActivity.class);
+                startActivity(intent);
             }
         });
 
